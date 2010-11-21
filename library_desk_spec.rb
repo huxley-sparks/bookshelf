7d7d55febfbac4a25eaf3861ecef51f24fd67882
Huxley <huxley.spark@gmail.com>
2010-11-21 15:18:27 -0800

    adding tests

---
+++ library_desk_spec.rb
@@ -0,0 +1,15 @@
+# Testing the library_desk
+require './library_desk'
+
+describe "Student" do
+
+	it "should have a name" do
+		pending("don't know how to test")
+	end
+
+	it "should create a new user" do
+		choice = 1
+	end
+
+
+end
